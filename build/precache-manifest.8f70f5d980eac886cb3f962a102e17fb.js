7dee266fd52dc8425f2e1be3f6276f9eb1e2f7e0
Mike Blakeway <mike@bluemoonwebsites.co.uk>
2019-06-26 11:44:20 +0100

    added /build folder to netlify branch

---
+++ build/precache-manifest.8f70f5d980eac886cb3f962a102e17fb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "528a497a37f7bc4732d17a0b92234409",
+    "url": "/mayan_react/index.html"
+  },
+  {
+    "revision": "45308ad1b5eb1cecb463",
+    "url": "/mayan_react/static/css/2.e53c13c6.chunk.css"
+  },
+  {
+    "revision": "9de7201395d98bcfd844",
+    "url": "/mayan_react/static/css/main.0f296880.chunk.css"
+  },
+  {
+    "revision": "45308ad1b5eb1cecb463",
+    "url": "/mayan_react/static/js/2.9e2a439a.chunk.js"
+  },
+  {
+    "revision": "9de7201395d98bcfd844",
+    "url": "/mayan_react/static/js/main.9759580e.chunk.js"
+  },
+  {
+    "revision": "49bf115bbfdf182ba20b",
+    "url": "/mayan_react/static/js/runtime~main.6f2fbf61.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/mayan_react/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/mayan_react/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/mayan_react/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/mayan_react/static/media/slick.f97e3bbf.svg"
+  }
+]);
